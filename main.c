dd5b7cc36d0b5b6bc01843042705380a4d48667b
youn1575 <youn461515@gmail.com>
2023-09-21 15:54:33 +0900

    230921_01

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 int main(int argc, char *argv[]) {
-int i, j;
-float x, y;
-
-	scanf("%d %d %f %f", &i, &j, &x, &y);
-	printf("%d %d %f %f", i, j, x, y);
-	getchar();
-
+	
+	int input_int;
+	float input_float;
+	
+	printf("enter an integer: ");
+	scanf("%d", &input_int);
+	
+	printf("enter a float: ");
+	scanf("%f", &input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float);
 	return 0;
 }
