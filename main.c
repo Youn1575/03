ca249c32d156890aab54dbf5c9dd58b62f108858
youn1575 <youn461515@gmail.com>
2023-09-21 15:36:31 +0900

    Initial commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+int i, j;
+float x, y;
+
+	scanf("%d %d %f %f", &i, &j, &x, &y);
+	printf("%d %d %f %f", i, j, x, y);
+	getchar();
+
+	return 0;
+}
